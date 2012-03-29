e3e1f24539a1f65c8adff94b5d8653c348dbe175
Anand Chitipothu <anandology@gmail.com>
2012-03-29 20:00:56 +0530

    The webapp to expose the liveweb proxy.
    Handles gzip compression etc, as the warc library doesn't support it
    yet.

--- liveweb/webapp.py
+++ liveweb/webapp.py
@@ -2,6 +2,8 @@
 """
 
 import arc_proxy
+import gzip
+from cStringIO import StringIO
 
 class application:
     """WSGI application for liveweb proxy.
@@ -10,8 +12,25 @@ class application:
         self.environ = environ
         self.start_response = start_response
         
+    def parse_request(self):
+        self.method = self.environ['REQUEST_METHOD']
+        self.url = self.environ['PATH_INFO']
+        
     def __iter__(self):
+        self.parse_request()
+        
         status = '200 OK'
-        response_headers = [('Content-type', 'text/plain')]
+        response_headers = [
+            ('Content-type', 'application/x-arc-record')
+        ]
+        
+        record = arc_proxy.get(self.url)
         self.start_response(status, response_headers)
-        yield "hello world!\n"
+        
+        f = StringIO()
+        
+        gz = gzip.GzipFile(fileobj=f, mode="wb")
+        record.write_to(gz, version=1)
+        gz.close()
+        
+        yield f.getvalue()
