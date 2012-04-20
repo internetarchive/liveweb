d132a6d45d5c1343af2a9245d7859b7b0cddd614
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-04-20 18:26:03 +0530

    Added file_pool implementation and tests

---
+++ liveweb/tests/test_filepool.py
@@ -0,0 +1,121 @@
+import glob
+import os
+
+def test_creation(pooldir):
+    """
+    Tests to see if the file pool is created properly.
+
+    Creates a pool and then walks through the pool directory to see if
+    the expected files are there.
+    """
+    from ..file_pool import FilePool
+    
+    # Create the pool
+    pool = FilePool(pooldir, pattern = "test-%(seq)s", max_files = 10, max_file_size = 10)
+    
+    # Get files in pool directory. 
+    pool_files = set(glob.glob(pooldir + "/*"))
+
+    # Check if this is the same as what we expect
+    expected_files = set(["%s/test-%05d"%(pooldir,x) for x in range(0,10)])
+    
+    assert expected_files == pool_files
+
+def test_get_return(pooldir):
+    """
+    Tests to see if get_file return_file work as expected. 
+
+    gets files, checks the number of files in the queue, returns them
+    and checks again.
+    """
+    
+    from ..file_pool import FilePool
+    
+    # Create the pool
+    pool = FilePool(pooldir, pattern = "test-%(seq)s", max_files = 10, max_file_size = 10)
+
+    fps = []
+
+    assert len(pool.queue.queue) == 10
+
+    for i in range(1, 6):
+        fps.append(pool.get_file())
+        assert len(pool.queue.queue) == 10 - i
+
+    assert len(pool.queue.queue) == 5
+        
+    for i in range(1, 6):
+        pool.return_file(fps.pop())
+        assert len(pool.queue.queue) == 5 + i
+
+    assert len(pool.queue.queue) == 10
+
+def test_max_file_size(pooldir):
+    """
+    Tests to see if the files are closed and released when the maximum
+    file size is reached and the file is returned.
+    """
+    from ..file_pool import FilePool
+    
+    # Create the pool
+    pool = FilePool(pooldir, pattern = "test-%(seq)s", max_files = 10, max_file_size = 10)
+    
+    fp = pool.get_file()
+    fp.write("test" * 100) # Max size has been exceeded. File should
+    pool.return_file(fp)   # get removed from pool when returned.
+
+    pool_files = set(x.fp.name for x in pool.queue.queue)
+    expected_files = set(["%s/test-%05d"%(pooldir,x) for x in range(1,11)])
+
+    assert expected_files == pool_files
+    
+    
+def test_close_pool(pooldir):    
+    """
+    Makes sure that the pool is emptied when closed. 
+
+    """
+    from ..file_pool import FilePool
+    
+    # Create the pool
+    pool = FilePool(pooldir, pattern = "test-%(seq)s", max_files = 10, max_file_size = 10)
+    
+    pool.close()
+    
+    assert len(pool.queue.queue) == 0
+
+
+def test_member_file_context(pooldir):
+    """
+    Tests the context manager behaviour of the MemberFile object.
+    """
+        
+    from ..file_pool import FilePool
+    
+    # Create the pool
+    pool = FilePool(pooldir, pattern = "test-%(seq)s", max_files = 10, max_file_size = 10)
+    
+    assert len(pool.queue.queue) == 10
+
+    with pool.get_file() as f:
+        assert len(pool.queue.queue) == 9
+        name = f.name
+        f.write("Hello")
+    
+    assert len(pool.queue.queue) == 10
+    pool.close()
+
+    assert open(name).read() == "Hello"
+    
+
+
+        
+
+
+        
+        
+    
+    
+    
+
+    
