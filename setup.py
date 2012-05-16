b3255ec52bb94b0f730b7dea5b9bcd0ab5ece99a
Anand Chitipothu <anandology@gmail.com>
2012-05-16 17:13:40 +0530

    Added dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 
 from setuptools import setup
 
+requirements = [line.strip() for line in open("requirements.txt")]
+
 setup(
     name="liveweb",
     version="2.0-dev",
@@ -9,12 +11,13 @@ setup(
     author="Internet Archive",
     author_email="info@archive.org",
     url="http://github.com/internetarchive/liveweb",
-    packages=["liveweb"],
+    packages=["liveweb", "liveweb.tools"],
     platforms=["any"],
     entry_points={
         "console_scripts": [
             "liveweb-proxy=liveweb.cli:main"
         ]
-    }
+    },
+    install_requires=requirements
 )
 
